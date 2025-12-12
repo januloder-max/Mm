17eeb6f022bb415b5144d5809f26b03e799f6e36
januloder-max <januloder@gmail.com>
2025-12-12 21:07:20 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,16 +1,9 @@
-
 mkdir -p .devcontainer
-
 cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "features": {
-        "ghcr.io/devcontainers/features/sshd:1": {
-            "version": "latest"
-        }
-    },
-    "postStartCommand": "REPO_NAME=\$(basename \$(git rev-parse --show-toplevel)); python3 /workspaces/\$REPO_NAME/g.py",
+    "postStartCommand": "python3 /workspaces/Telegram-Bot/Danger.py",
     "customizations": {
         "vscode": {
             "settings": {
@@ -25,7 +18,5 @@ cat <<EOL > .devcontainer/devcontainer.json
 EOL
 
 git add .devcontainer/devcontainer.json
-
-git commit -m "Add postStartCommand to run Python script automatically with repo name detection"
-
+git commit -m "Add postStartCommand to run Python script automatically"
 git push origin main
